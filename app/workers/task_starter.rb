fda1b7856526e6bddc3a4ad06f9aace638585ea8
Nik Petersen <demersus@gmail.com>
2015-05-27 09:15:19 -0700

    removed rock sync from task starter

--- app/workers/task_starter.rb
+++ app/workers/task_starter.rb
@@ -7,9 +7,9 @@ class TaskStarter
     default_q = Sidekiq::Queue.new()
     conversion_q = Sidekiq::Queue.new('conversion')
     arena_q = Sidekiq::Queue.new('arena')
-    rock_q = Sidekiq::Queue.new('rock')
+    #rock_q = Sidekiq::Queue.new('rock')
 
-    if default_q.size > 0 || retries.size > 0 || conversion_q.size > 0 || arena_q.size > 0 || rock_q.size > 0
+    if default_q.size > 0 || retries.size > 0 || conversion_q.size > 0 || arena_q.size > 0# || rock_q.size > 0
       puts 'jobs are running; quiting'
       return
     end
@@ -20,7 +20,7 @@ class TaskStarter
       ConversionStarter.perform_async
     else
       Rails.cache.write('task:conversion',true)
-      puts 'queueing arena & rock sync'
+      puts 'queueing arena sync'
       ArenaSync.perform_async
       #RockSync.perform_async
     end
