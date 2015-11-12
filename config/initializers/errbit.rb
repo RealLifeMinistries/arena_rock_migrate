35e5e06bfadd5f6e8353b6c1f6beace78bf60abc
Nik Petersen <npetersen@viciware.com>
2015-11-12 14:08:54 -0800

    added errbit

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,5 +1,5 @@
 Airbrake.configure do |config|
-  config.api_key = '9259046b8a70a3e477587851a4ae0fa3'
+  config.api_key = '16dfd5a3a0cc0688235c1744de4716a0'
   config.host    = 'errbit.viciware.com'
   config.port    = 80
   config.secure  = config.port == 443
