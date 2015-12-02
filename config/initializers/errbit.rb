8efd4aeedafcf5988dce4a790051aab60f35e7ab
Nik Petersen <npetersen@viciware.com>
2015-12-02 11:53:48 -0800

    changed errbit config to use ENV

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,6 +1,8 @@
-Airbrake.configure do |config|
-  config.api_key = '16dfd5a3a0cc0688235c1744de4716a0'
-  config.host    = 'errbit.viciware.com'
-  config.port    = 80
-  config.secure  = config.port == 443
+if ENV['ERRBIT_KEY'] && ENV['ERRBIT_HOST']
+   Airbrake.configure do |config|
+    config.api_key = ENV['ERRBIT_KEY'] 
+    config.host    = ENV['ERRBIT_HOST']
+    config.port    = ENV.fetch('ERRBIT_PORT',80)
+    config.secure  = config.port == 443
+  end
 end
