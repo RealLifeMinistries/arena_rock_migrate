08f44e78c80e94e1342aa9246c6fbb99bb4263ef
Kyle Craviotto <im.kyle@gmail.com>
2016-04-10 17:38:53 -0700

    Merge branch 'sqlnew'
    # Conflicts:
    #	.idea/workspace.xml

--- app/models/arena_profile.rb
+++ app/models/arena_profile.rb
@@ -47,7 +47,7 @@ class ArenaProfile < ArenaBase
       if parent_profile_id?
         @rock.ParentGroupId ||= parent.mapped_id
       else
-        @rock.ParentGroupId ||= profile_type_record.default_rock_parent_group_id
+        @rock.ParentGroupId ||= default_rock_parent_group_id
       end
       @rock.GroupTypeId ||= get_group_type_id#profile_type_record.mapping.#profile_type_record.mapped_id
       @rock.Name ||= profile_name
@@ -66,11 +66,11 @@ class ArenaProfile < ArenaBase
   
   def get_group_type_id
     case profile_type_record.id
-      when 1
+      when ArenaProfileType::PARTICIPANT
         return RockGroupType::PARTICIPANT_MINISTRY
-      when 2
+      when ArenaProfileType::SERVING
         return RockGroupType::SERVING
-      when 4
+      when ArenaProfileType::CLASSES_EVENTS
         if [3258, 3075, 1282, 1676, 2428, 2801, 2862, 2772, 1279, 1378, 2824, 1149, 1779, 1843, 2460, 1462, 1406, 3311, 1886, 1833, 3226, 1456, 1831, 1700, 3041, 1464, 1283, 1674, 1412, 1448, 1626, 3152, 3044].include? parent_profile_id
           return RockGroupType::CLASS
         end
@@ -79,6 +79,23 @@ class ArenaProfile < ArenaBase
         end
       else
         return nil
+    end
+  end
+  def default_rock_parent_group_id
+    case profile_type_record.id
+      when ArenaProfileType::PARTICIPANT
+        return RockGroup::PARTICIPANT_MINISTRY
+      when ArenaProfileType::SERVING
+        return RockGroup::SERVING_MINISTRY
+      when ArenaProfileType::CLASSES_EVENTS
+        if [3258, 3075, 1282, 1676, 2428, 2801, 2862, 2772, 1279, 1378, 2824, 1149, 1779, 1843, 2460, 1462, 1406, 3311, 1886, 1833, 3226, 1456, 1831, 1700, 3041, 1464, 1283, 1674, 1412, 1448, 1626, 3152, 3044].include? id
+          return RockGroup::CLASS
+        end
+        if [1281, 2695, 1731, 1421, 2554, 2643, 2241, 1556, 2251, 1973, 2966, 1946, 3022, 1679, 1753].include? id
+          return RockGroup::EVENTS
+        end
+      else
+        nil
     end
   end
 
