faedcff99d6c79c6645ac64683f4da77808e0285
Kyle Craviotto <im.kyle@gmail.com>
2016-05-03 11:23:44 -0700

    fix some note issues

--- app/models/arena_notes.rb
+++ app/models/arena_notes.rb
@@ -8,18 +8,18 @@ class ArenaNotes < ArenaBase
     if (history_type_luid == 355)
       map = mapping || build_mapping
 
-      rock_note = mapping.rock_record ||= RockNote.find_or_initialize_by({
-        NoteTypeId: 21,
-        ModifiedDateTime: date_modified
-      })
-      rock_note.Guid = SecureRandom.uuid
-      rock_note.IsSystem = false
-      rock_note.IsPrivateNote = false
-      rock_note.IsAlert = false
-      rock_note.Caption = ''
-      rock_note.EntityId = person.mapped_record.Id
-      rock_note.Text = "Entered by: #{modified_by}<br>#{history}"
-      rock_note.CreatedDateTime = date_created
+      rock_note = mapping.rock_record ||= RockNote.new
+      rock_note.NoteTypeId ||= 21
+      rock_note.ModifiedDateTime ||= date_modified
+      rock_note.Guid ||= SecureRandom.uuid
+      rock_note.IsSystem ||= false
+      rock_note.IsPrivateNote ||= false
+      rock_note.IsAlert ||= false
+      rock_note.Caption ||= ''
+      rock_note.EntityId ||= person.mapped_record.Id
+      rock_note.Text ||= "Entered by: #{modified_by}
+                        #{history}"
+      rock_note.CreatedDateTime ||= date_created
       rock_note.save!
       map.save!
     end
